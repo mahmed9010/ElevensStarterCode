2012ce02691890541d6738fc0b59ed527f03fd0e
BT_1N3_05 <mahmed9010@bths.edu>
2019-01-09 11:38:42 -0500

    fgtrfgvchjgf

--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -5,6 +5,15 @@ package Activity1;
  */
 public class CardTester {
     public static void main(String[] args){
-
+        Card card1 = new Card("7","diamond",7);
+        Card card2 = new Card("7","diamond",7);
+        Card card3 = new Card("Jack","spades",11);
+        System.out.println(card1.toString());
+        System.out.println(card2.toString());
+        System.out.println(card3.toString());
+        System.out.println(card1.matches(card2));
+        System.out.println(card1.matches(card3));
+        System.out.println(card2.matches(card1));
+        System.out.println(card2.matches(card3));
     }
 }
